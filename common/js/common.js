0224b7f00200df80aba0b68b18bce75e4ab0869d
Rey <zh_rey@163.com>
2019-07-08 11:58:21 +0800

    提交

--- common/js/common.js
+++ common/js/common.js
@@ -14,16 +14,38 @@ $(function(){
     });
   }
 
-  $('body').on('click','.back.iconleft',function(){ // 返回操作
+  $('body').on('click','.back.iconleft, .back.iconclose',function(){ // 返回操作
     window.history.go(-1);
   });
 
   //全局弹框
-  window.$alert = function(str, fun) {
-
+  window.$loading = function() {
+    $('body').append('<div class="loading model"></div>');
   }
-  window.$confirm = function(str, fun) {
-    
+  window.$endloading = function() {
+    $('.loading.model').remove();
+  }
+  window.$alert = function(title, str, fun) {
+    title = title || 'YiLiFang'
+    $('body').append('<div class="model_alert"><div class="alert"><div class="title">'+title+'</div><div class="info">'+str+'</div><div class="btns"><span class="btnn ok">OK</span></div></div></div>');
+    setTimeout(function(){
+      $('.model_alert').on('click', '.ok', function(){
+        typeof(fun) === 'function' && fun();
+        $(this).parents('.model_alert').remove();
+      });
+    }, 0);
+  }
+  window.$confirm = function(title, str, fun) {
+    title = title || 'YiLiFang'
+    $('body').append('<div class="model_confirm"><div class="confirm"><div class="title">'+title+'</div><div class="info">'+str+'</div><div class="btns"><span class="btnn cancel">Cancel</span><span class="btnn ok">OK</span></div></div></div>');
+    setTimeout(function(){
+      $('.model_confirm').on('click', '.cancel', function(){
+        $(this).parents('.model_confirm').remove();
+      }).on('click', '.ok', function(){
+        typeof(fun) === 'function' && fun();
+        $(this).parents('.model_confirm').remove();
+      });
+    }, 0);
   }
   var msgTimer = null;
   window.$message = function(str, type) { //顶部提示弹框
